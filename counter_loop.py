2ade7176a1eb12b729deb7734bfd60f0e71cf4e7
PaulSvosve <100382486+PaulSvosve@users.noreply.github.com>
2023-07-04 19:22:16 -0500

    Add files via upload

---
+++ counter_loop.py
@@ -0,0 +1,8 @@
+# Initialize counter
+counter = 1
+# Iterate the loop 9 times
+while counter < 10:
+    # Print the counter value
+    print ("%d" % counter)
+    # Increment the counter
+    counter = counter + 1
